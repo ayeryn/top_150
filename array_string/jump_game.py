f520ec6cd77fe58d01a913ffe0cf116c4180c0b9
ayeryn <erynli@utexas.edu>
2024-06-19 17:14:10 -0500

    update brute

--- array_string/jump_game.py
+++ array_string/jump_game.py
@@ -1,4 +1,11 @@
 # 55
+
+
+def jump_game(nums: list[int]) -> bool:
+    # TODO:
+    return False
+
+
 def jump_game_brute(nums: list[int]) -> bool:
     n = len(nums)
     reach = [False] * n
@@ -8,7 +15,8 @@ def jump_game_brute(nums: list[int]) -> bool:
     # Exclude last index as it's the destination
     for i in range(n - 1):
         if reach[i]:
-            for j in range(1, nums[i] + 1):  # This could be super large
+            for j in range(1, min(nums[i] + 1, n - i)):
+                # This loop could be super large
                 reach[min(i + j, n - 1)] = True
 
     return reach[-1]

c90f5d47d171600747701b17386e8fbcb86a8b6c
ayeryn <erynli@utexas.edu>
2024-06-19 22:32:05 -0500

    brute

---
+++ array_string/jump_2.py
@@ -0,0 +1,17 @@
+# 45
+
+
+def jump_brute(nums: list[int]) -> int:
+    n = len(nums)
+    dp = [float("inf")] * n
+    dp[0] = 0
+
+    for i in range(n - 1):
+        for j in range(i + 1, min(n, i + nums[i] + 1)):
+            # Update reachable indexes from i
+            dp[j] = min(dp[i] + 1, dp[j])
+            if j == n - 1:
+                # return if we reach the end early
+                return dp[n - 1]
+
+    return dp[-1]

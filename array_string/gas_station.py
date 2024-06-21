c680c666d97e027801a9cbd284d95a2914079f99
ayeryn <erynli@utexas.edu>
2024-06-21 16:37:19 -0500

    use prefix sum

--- array_string/gas_station.py
+++ array_string/gas_station.py
@@ -1,3 +1,30 @@
+def gas_station(gas, cost):
+    prefix = []
+    p_sum = 0
+    n = len(gas)
+
+    for i in range(n):
+        distance = gas[i] - cost[i]
+        prefix.append(distance)
+        p_sum += distance
+
+    if p_sum < 0:
+        return -1
+
+    left = 0
+    total = 0
+    start = 0
+    for i in range(n):
+        total += prefix[i]
+
+        if total < 0:
+            left += total
+            total = 0
+            start = i + 1
+
+    return start
+
+
 def gas_station_brute(gas: list[int], cost: list[int]) -> int:
     n = len(gas)
 

f63ebbfd9cd14c48a215c9ad2cb0a9e63b9e61b9
ayeryn <erynli@utexas.edu>
2024-06-21 15:00:22 -0500

    use hashmap and list for O(1) getRandom

--- array_string/RandomSet.py
+++ array_string/RandomSet.py
@@ -4,21 +4,31 @@ import random
 class RandomizedSet:
 
     def __init__(self):
-        self.s = set()
+        self.m = {}
+        self.l = []
 
     def insert(self, val: int) -> bool:
-        if val in self.s:
+        if val in self.m:
             return False
         else:
-            self.s.add(val)
+            self.m[val] = len(self.l)
+            self.l.append(val)
             return True
 
     def remove(self, val: int) -> bool:
-        if val in self.s:
-            self.s.remove(val)
+        if val in self.m:
+            last_val = self.l.pop()  # get last val
+            if val != last_val:
+                # Replace removed ele with last ele
+                ind = self.m[val]  # get removed index
+                self.m[last_val] = ind  # update last_val index
+                self.l[ind] = last_val  # update list
+
+            del self.m[val]  # delete val from hashmap
             return True
         else:
             return False
 
     def getRandom(self) -> int:
-        return random.choice(list(self.s))
+        random_ind = random.randint(0, len(self.l) - 1)
+        return self.l[random_ind]

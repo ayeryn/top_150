94c690914c29b9b5115172b71e4b9f115596a0fc
ayeryn <erynli@utexas.edu>
2024-06-25 12:06:43 -0500

    use math.ceil to determine how many spaces to insert at each slot

--- array_string/text_justification.py
+++ array_string/text_justification.py
@@ -1,36 +1,51 @@
 # 68
+import math
 
 
 def text_justification(words: list[str], maxWidth: int) -> list[list[str]]:
     def process_line(line, count, last_line):
         if len(line) == 1:
+            # Only word is always left-justified
             return line[0] + " " * (maxWidth - len(line[0]))
 
         spaces = maxWidth - count
+        slots = len(line) - 1
         ans = ""
+
         if last_line:
             # left aligned
             for i in range(len(line) - 1):
                 ans += line[i] + " "
-            return ans + line[-1] + (spaces - (len(line) - 1)) * " "
+            return ans + line[-1] + (spaces - slots) * " "
 
         else:
-            s = spaces // (len(line) - 1)
-            ans = line[0] + " " * (s + spaces % (len(line) - 1))
+            """
+            Since left slots get more spaces when uneven, we can keep getting ceil(spaces_left / slots_left) until slots_left reaches 0
+            """
+            ls = math.ceil(spaces / slots)
 
-            for i in range(1, len(line) - 1):
-                ans += line[i] + s * " "
+            for i in range(slots):
+                ans += line[i] + " " * ls
+                spaces -= ls
+                slots -= 1
+                if slots:  # update ls until slots reaches 0
+                    ls = math.ceil(spaces / slots)
 
-            return ans + line[-1]
+            return ans + line[-1]  # append last word to ans
 
     ans = []
-    line = []
-    count = 0
+    line = [] # words on one line
+    count = 0 # number of characters only
     for w in words:
-        # print(f"w = {w}, line = {line}, c = {count}, op = {count + len(line) + len(w) - 1}")
         if line != "":
             if count + len(line) + len(w) > maxWidth:
-                # wrap up current line and start nl
+                """
+                Wrap up current line and start new line when
+                - # of characters + 
+                - spaces needed if appending current word =
+                - len(w)
+                is > maxWidth
+                """
                 ans.append(process_line(line, count, False))
                 line = [w]
                 count = len(w)

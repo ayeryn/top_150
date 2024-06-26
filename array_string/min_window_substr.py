23735ec2d0f0156f2d5772e0b6794046e71e0a6f
ayeryn <erynli@utexas.edu>
2024-06-25 21:57:44 -0500

    O analysis

--- array_string/min_window_substr.py
+++ array_string/min_window_substr.py
@@ -2,7 +2,8 @@
 from collections import defaultdict
 
 
-def min_window_substr(s: str, t: str) -> str:
+def min_window_substr(s: str, t: str) -> str:  # O(M+N)
+    # M - len(s), N = len(t)
     if s == t:
         return s
     if len(s) < len(t):
@@ -11,12 +12,12 @@ def min_window_substr(s: str, t: str) -> str:
     ans = ""
     left = 0
     count = defaultdict(int)
-    for c in t:
+    for c in t:  # O(N)
         count[c] += 1
     d = defaultdict(int)
     has = 0  # keep track of how many chars in t we have found
 
-    for right in range(len(s)):
+    for right in range(len(s)):  # O(M)
         if s[right] in count:
             d[s[right]] += 1
             has += d[s[right]] <= count[s[right]]  # update has

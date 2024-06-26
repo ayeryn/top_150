6ac69d68d0c9d6d1db3e6005526ab9cf5b2abc64
ayeryn <erynli@utexas.edu>
2024-06-26 13:04:37 -0500

    complete

--- matrix/rotate_image.py
+++ matrix/rotate_image.py
@@ -9,36 +9,32 @@ def rotate_image(matrix: list[list[int]]) -> None:
     l, r = 0, n - 1
 
     while l < r:
-        for pos in range(l, r):
+        for pos in range(r - l):
             i, j = l, l + pos
             curr = matrix[i][j]
 
             # left to right
-            i, j = pos, r
+            i, j = l + pos, r
             temp = matrix[i][j]
             matrix[i][j] = curr
-            print(f"({i}, {j}): {temp} => {curr}")
             curr = temp
 
             # right to down
             i, j = r, r - pos
             temp = matrix[i][j]
             matrix[i][j] = curr
-            print(f"({i}, {j}): {temp} => {curr}")
             curr = temp
 
             # down to left
             i, j = r - pos, l
             temp = matrix[i][j]
             matrix[i][j] = curr
-            print(f"({i}, {j}): {temp} => {curr}")
             curr = temp
 
             # left to up
             i, j = l, l + pos
             temp = matrix[i][j]
             matrix[i][j] = curr
-            print(f"({i}, {j}): {temp} => {curr}\n")
             curr = temp
 
         l += 1

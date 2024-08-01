61d9a5aec72deeea0e453c09c06428b0e0f30ef2
ayeryn <erynli@utexas.edu>
2024-08-01 10:47:33 -0500

    enhance word ladder

--- graph/word_ladder.py
+++ graph/word_ladder.py
@@ -1,40 +1,21 @@
 # 127
 
-from collections import deque
+from collections import deque, defaultdict
 
 
 def ladder(beginWord: str, endWord: str, wordList: list[str]) -> int:
     if beginWord == endWord:
         return 0
 
-    wordList = set(wordList)
-
-    def process_chars():
-        # Put all valid chars in a set
-        s = set()
-        for word in wordList:
-            for c in word:
-                if c not in s:
-                    s.add(c)
-        return s
-
-    valid_chars = process_chars()
+    ladder = defaultdict(list)
+    # Build adjacency graph
+    # Utilize "*" and avoid checking possible chars for position
+    for word in wordList:
+        for i in range(len(word)):
+            pattern = word[:i] + "*" + word[i + 1 :]
+            ladder[pattern].append(word)
 
     seen = set([beginWord])
-
-    def change(word):
-        ans = []
-        for i in range(len(word)):
-            for c in valid_chars:
-                if c != word[i]:
-                    temp = word[:i] + c + word[i + 1 :]
-                    if temp not in seen and temp in wordList:
-                        # Add possible changed word to ans
-                        ans.append(temp)
-                        seen.add(temp)
-
-        return ans
-
     graph = deque([beginWord])
     steps = 1  # First word counts as part of the ladder
 
@@ -42,11 +23,16 @@ def ladder(beginWord: str, endWord: str, wordList: list[str]) -> int:
         n = len(graph)
         for _ in range(n):
             word = graph.popleft()
+            if word == endWord:
+                return steps
 
-            for w in change(word):
-                if w == endWord:
-                    return steps + 1
-                graph.append(w)
+            for i in range(len(word)):
+                pattern = word[:i] + "*" + word[i + 1 :]
+                for w in ladder[pattern]:
+                    # Check ladder patten graph for available words
+                    if w != word and w not in seen:
+                        graph.append(w)
+                        seen.add(w)
 
         steps += 1
 

460516f302eff70864147912128f8823dc2f4ad7
ayeryn <erynli@utexas.edu>
2024-08-06 10:41:38 -0500

    LIS - 1D mem N*N runtime

--- dp/LIS.py
+++ dp/LIS.py
@@ -1,6 +1,21 @@
 # 300
 
 
+def LIS(nums: list[int]):
+    if len(nums) == 1:
+        return 1
+
+    ans = 1
+    dp = [1 for _ in range(len(nums))]
+    for i in range(len(nums) - 2, -1, -1):
+        for j in range(i + 1, len(nums)):
+            if nums[i] < nums[j]:
+                dp[i] = max(dp[i], 1 + dp[j])
+        ans = max(ans, dp[i])
+
+    return ans
+
+
 def LIS_2D(nums: list[int]) -> int:
     def dp(i, seq):
         if i == len(nums):

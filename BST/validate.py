2d114544cad291193b7d6de1a0ea984b7eb98f47
ayeryn <erynli@utexas.edu>
2024-08-07 11:33:21 -0500

    clean up

--- BST/validate.py
+++ BST/validate.py
@@ -10,24 +10,37 @@ def validate(root: TreeNode) -> bool:
         left = right = True
         lmax = rmin = node.val
 
+        """
+        For node to be a valid BST:
+        1. left subtree is valid
+        2. right subtree is valid
+        3. max value in left subtree < node.val
+        4. min value in right subtree > node.val
+        """
+
         if node.left:
             if node.left.val >= node.val:
                 left = False
-            llmax, llmin, left = dfs(node.left)
-            if llmax >= node.val:
+
+            l_lmax, l_rmin, left = dfs(node.left)
+            if l_lmax >= node.val:
                 left = False
-            lmax = max(llmax, lmax, rmin, llmin)
-            rmin = min(llmax, lmax, rmin, llmin)
+
+            lmax = max(lmax, rmin, l_lmax, l_rmin)
+            rmin = min(lmax, rmin, l_lmax, l_rmin)
 
         if node.right:
             if node.right.val <= node.val:
                 right = False
-            rlmax, rrmin, right = dfs(node.right)
-            if rrmin <= node.val:
-                right = False
-            lmax = max(rlmax, lmax, rmin, rrmin)
-            rmin = min(rlmax, lmax, rmin, rrmin)
 
+            r_lmax, r_rmin, right = dfs(node.right)
+            if r_rmin <= node.val:
+                right = False
+
+            lmax = max(lmax, rmin, r_lmax, r_rmin)
+            rmin = min(lmax, rmin, r_lmax, r_rmin)
+
+        # Get max and min value of current subtree
         return max(lmax, rmin), min(lmax, rmin), left and right
 
     return dfs(root)[2]

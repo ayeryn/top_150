b52e660de5a8c0a564021d32957e51713fd13692
ayeryn <erynli@utexas.edu>
2024-08-26 11:51:00 -0500

    factorial notes

--- math/factorial.py
+++ math/factorial.py
@@ -1,7 +1,24 @@
 # 172
 
 
-def factorial(n: int) -> int:
+def factorial(n):
+    """
+    TODO: implement in code
+    - 2 * 5 -> 10
+    - 10 -> 10
+
+    For every 1-10 interval, we gain 2 0's bcz
+    - we have one 5
+    - we have one 10
+
+    Extra zeroes:
+    - powers of 5
+    - multiples of powers of 5
+    - multiples of 50
+    """
+
+
+def factorial_brute(n: int) -> int:
     product = 1
 
     while n:  # Calculate factorial
